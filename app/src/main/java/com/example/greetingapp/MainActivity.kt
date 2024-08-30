63a2cf1ebe782b19fa03a612c3ad01cadea6ceff
Haroon Waheed <haroonwaheed002@gmail.com>
2024-08-30 21:18:09 +0500

    app icon is added

--- app/src/main/java/com/example/greetingapp/MainActivity.kt
+++ app/src/main/java/com/example/greetingapp/MainActivity.kt
@@ -6,7 +6,6 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,14 +13,13 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
 
         val greetingText = findViewById<TextView>(R.id.tvGreetings)
         val userName = findViewById<TextView>(R.id.etName)
@@ -29,44 +27,35 @@ class MainActivity : AppCompatActivity() {
         val male = findViewById<RadioButton>(R.id.rdBtn1)
         val female = findViewById<RadioButton>(R.id.rdbtn2)
         val rdGroup = findViewById<RadioGroup>(R.id.rdGroup)
-        var enteredName = ""
 
         submitButton.setOnClickListener {
-            enteredName = userName.text.toString()
-            if(enteredName == ""){
-                val message = "Welcome to Android!"
-                greetingText.text = message
-                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT).show()
-            }
-            else
-            {
-                if(male.isChecked)
-                {
-                    val message = "Welcome! Mr $enteredName!"
-                    greetingText.text = message
-                    userName.text = ""
-                    male.isChecked = false
-                    female.isChecked = false
-                }
-                else if(female.isChecked)
-                {
-                    val message = "Welcome! Ms $enteredName!"
-                    greetingText.text = message
-                    userName.text = ""
-                    male.isChecked = false
-                    female.isChecked = false
-                }
-                else
-                {
-                    val message = "Please Select the Gender first"
-                    greetingText.text = message
-                    Toast.makeText(this@MainActivity, "Please Select the Gender first", Toast.LENGTH_SHORT).show()
-                    userName.text = ""
-                    male.isChecked = false
-                    female.isChecked = false
-                }
+            val enteredName = userName.text.toString().trim()
 
+            if (enteredName.isEmpty()) {
+                greetingText.text = "Welcome to Android!"
+                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                when {
+                    male.isChecked -> {
+                        greetingText.text = "Welcome! Mr $enteredName!"
+                        // Clear fields only if the input is valid
+                        rdGroup.clearCheck()
+                        userName.text = ""
+                    }
+                    female.isChecked -> {
+                        greetingText.text = "Welcome! Ms $enteredName!"
+                        // Clear fields only if the input is valid
+                        rdGroup.clearCheck()
+                        userName.text = ""
+                    }
+                    else -> {
+                        greetingText.text = "Hello $enteredName, please select your gender"
+                        Toast.makeText(this@MainActivity, "Please Select the Gender", Toast.LENGTH_SHORT).show()
+                        // Do not clear the username field
+                    }
+                }
             }
         }
     }
 }
+
